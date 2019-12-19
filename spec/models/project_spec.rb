cfbedcf4d0e94b176c3f28543c5db4ab51a47e41
adnrbp <adnrbp@users.noreply.github.com>
2019-12-18 19:20:11 -0500

    [model] Task and Project: add tasks to a project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Project do
   it "considers a project with no tasks to be done" do
+    # Arrange
     project = Project.new
+    # Assert
     expect(project.done?).to be_truthy
+  end
+
+  it "knows that a project with an incomplete task is not done" do
+    # Arrange; Given
+    project = Project.new
+    task = Task.new
+    # Act; When
+    project.tasks << task
+    # Assert; Then
+    expect(project.done?).to be_falsy
   end
 end
