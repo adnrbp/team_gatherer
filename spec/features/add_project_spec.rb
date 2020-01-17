1770e1df5a36b2371fcda135ebd927a106e64b56
adnrbp <adnrbp@users.noreply.github.com>
2020-01-16 19:12:19 -0500

    [view] Project: index page style

--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -8,9 +8,14 @@ describe "adding projects" do
     
     click_on("Create Project")
     visit projects_path
+
+    @project = Project.find_by_name("PRoject Runway")
     
-    expect(page).to have_content("Project Runway")
-    expect(page).to have_content("8")
+    expect(page).to have_selector(
+      "#project_#{@project.id} .name", text: "Project Runway")
+    expect(page).to have_selector(
+      "#project_#{@project.id} .total-size", text: "8")
+
 
   end
 end
